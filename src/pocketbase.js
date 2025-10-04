12278582f6eac2a8e5b45d8cfbd0a5b46f03f5e9
MurianMarc <uchenna.nwaogu@pau.edu.ng>
2025-10-04 01:02:26 +0100

    bug fixes

--- src/pocketbase.js
+++ src/pocketbase.js
@@ -1,7 +1,8 @@
 import PocketBase from 'pocketbase';
 
-const pb = new PocketBase('http://127.0.0.1:8090'); 
+const pb = new PocketBase('https://your-backend.onrender.com');
 export default pb;
+
 
 
 await pb.collection('users').create({
